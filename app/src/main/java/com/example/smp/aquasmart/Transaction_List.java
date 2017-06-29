d31070f9c7230335519d41bdbed81879ce7aa64e
Dipak <dipakbelsare@gmail.com>
2017-06-29 14:12:32 +0530

    Transaction list view update

--- app/src/main/java/com/example/smp/aquasmart/Transaction_List.java
+++ app/src/main/java/com/example/smp/aquasmart/Transaction_List.java
@@ -1,5 +1,7 @@
 package com.example.smp.aquasmart;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
@@ -10,28 +12,45 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.app.Activity;
+import android.widget.TextView;
 import android.widget.Toast;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class Transaction_List extends Activity {
-    Button Cust,AddCust,LogOt;
+    Button FromDate,ToDate,Show;
     ListView simpleList;
     String transURL="http://smartbizit.com/aquasmart/transaction_history.php";
     EditText editSearch;
     List<TransactionClass> transList;
+    private Calendar calendar;
+    private TextView dateView,dateViewTo;
+    private int year, month, day;
 
     private CustomAdapter mCustomAdapterTran;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transaction__list);
+
+        dateView = (TextView) findViewById(R.id.txtFromDate);
+//        dateViewTo = (TextView) findViewById(R.id.txtToDate);
+
+        calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        showDate(year, month+1, day);
+//        showDateTo(year, month+1, day);
 
         simpleList = (ListView) findViewById(R.id.simpleListView);
         editSearch=(EditText) findViewById(R.id.editSearch);
@@ -57,34 +76,77 @@ public class Transaction_List extends Activity {
             }
         });
 
-        Cust=(Button)findViewById(R.id.btnCustTrList);
-        AddCust=(Button)findViewById(R.id.btnAddCustTrList);
-        LogOt=(Button)findViewById(R.id.btnLogoutTrList);
-
-        Cust.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i=new Intent(Transaction_List.this,MainActivity.class);
-                startActivity(i);
-            }
-        });
-        AddCust.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i=new Intent(Transaction_List.this,CustomerAdd.class);
-                startActivity(i);
-            }
-        });
-
-        LogOt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i=new Intent(Transaction_List.this,LoginActivity.class);
-                startActivity(i);
-            }
-        });
+//        FromDate=(Button)findViewById(R.id.btnFromDate);
+//        ToDate=(Button)findViewById(R.id.btnToDate);
+//        Show=(Button)findViewById(R.id.btnShow);
+//
+//        FromDate.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+////                Intent i=new Intent(Transaction_List.this,MainActivity.class);
+////                startActivity(i);
+//            }
+//        });
+//        ToDate.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+////                Intent i=new Intent(Transaction_List.this,CustomerAdd.class);
+////                startActivity(i);
+//            }
+//        });
+//
+//        Show.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+////                Intent i=new Intent(Transaction_List.this,LoginActivity.class);
+////                startActivity(i);
+//            }
+//        });
 
     }
+
+    @SuppressWarnings("deprecation")
+    public void setDate(View view) {
+        showDialog(999);
+    }
+
+//    @SuppressWarnings("deprecation")
+//    public void setDateTo(View view) {
+//        showDialog(999);
+//    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        // TODO Auto-generated method stub
+        if (id == 999) {
+            return new DatePickerDialog(this,
+                    myDateListener, year, month, day);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener myDateListener = new
+            DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker arg0,
+                                      int arg1, int arg2, int arg3) {
+                    // TODO Auto-generated method stub
+                    // arg1 = year
+                    // arg2 = month
+                    // arg3 = day
+                    showDate(arg1, arg2+1, arg3);
+                }
+            };
+
+    private void showDate(int year, int month, int day) {
+        dateView.setText(new StringBuilder().append(day).append("/")
+                .append(month).append("/").append(year));
+    }
+
+//    private void showDateTo(int year, int month, int day) {
+//        dateViewTo.setText(new StringBuilder().append(day).append("/")
+//                .append(month).append("/").append(year));
+//    }
 
     private class GetHttpResponse extends AsyncTask<Void, Void, Void>
     {
